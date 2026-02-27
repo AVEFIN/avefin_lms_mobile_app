2aa37f1d8ab43aa77064f8315d15161498497a62
= <aahernandv@liverpool.com.mx>
2026-02-27 09:50:13 -0600

    remove unset weights

--- lib/theme/text_theme.dart
+++ lib/theme/text_theme.dart
@@ -6,7 +6,7 @@ class AppTextTheme {
     displayLarge: TextStyle(fontSize: 40, fontWeight: FontWeight.w500),
     displayMedium: TextStyle(fontSize: 32, fontWeight: FontWeight.w500),
 
-    titleLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+    titleLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
     titleMedium: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
 
     bodyLarge: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
